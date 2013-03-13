d6a831de15a637a74f3d6a2fbaecb3d590b9964f
rnml <none@none>
2013-03-12 23:14:04 -0400

    commented out ill-conceived org-mode representation code

--- org.ml
+++ org.ml
@@ -1,13 +1,21 @@
 open Core.Std
 
+(*
 type t = node list
  and node = Node of string * t
 with sexp
 
-let of_string text =
-  String.split ~on:'\n' text
-  |! List.map ~f:(fun line ->
+let rec of_lines lines =
+  List.map lines ~f:(fun line ->
     match String.chop_prefix line ~prefix:"* " with
     | None -> (false, line)
     | Some line -> (true, line))
-  |! List.group ~break:(fun header _ -> header)
+  |! List.group ~break:(fun header _ -> fst header)
+  |! List.map ~f:(function
+    | [] -> assert false
+    |
+
+let of_string text = String.split ~on:'\n' text |! of_lines
+
+List.map ~f:snd)
+*)
--- org.mli
+++ org.mli
@@ -1,7 +1,9 @@
 open Core.Std
 
+  (*
 type t = node list
  and node = Node of string * t
 with sexp
 
 val of_string : string -> t
+  *)
