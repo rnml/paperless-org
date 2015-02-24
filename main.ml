970e0df249c07c54136f1698851dde9408574da3
rnml <none@none>
2015-02-23 22:26:49 -0500

    bugfix.  parse-xml command

--- main.ml
+++ main.ml
@@ -21,14 +21,26 @@ let push_command =
     )
     (fun file dir () ->
        let p = Paperless.Org.load file in
-       let p = Paperless.normalize p in
        Paperless.Xml.save p dir)
+
+let parse_xml_command =
+  Command.basic ~summary:"parse xml file (for testing)"
+    Command.Spec.(
+      empty
+      +> anon ("XML-FILE" %: file)
+    )
+    (fun file () ->
+       Simple_xml.load file
+       |> Simple_xml.sexp_of_t
+       |> Sexp.to_string_hum
+       |> print_endline)
 
 let command =
   Command.group ~summary:"convert paperless lists between XML and Org-mode"
     [
       ("pull", pull_command);
       ("push", push_command);
+      ("parse-xml", parse_xml_command);
     ]
 
 let () = Command.run command
